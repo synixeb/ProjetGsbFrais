9baf1d0d2709f677f44b09c8d136dad7b57380b3
ebert <evan.bertin@lmdsio.fr>
2022-10-20 17:04:21 +0200

    init

--- resources/views/vues/formFrais.blade.php
+++ resources/views/vues/formFrais.blade.php
@@ -30,7 +30,7 @@
                 <button type="submit" class="btn btn-default btn-primary">
                     <span class="glyphicon glyphicon-ok"></span> Valider
                 </button>
-                &nbsp;
+
                 <button type="button" class="btn btn-default btn-primary"
                         onclick="javascript: window.location = '{{ url('/getListeFrais')}}';">
                     <span class="glyphicon glyphicon-remove"></span> Annuler</button>
